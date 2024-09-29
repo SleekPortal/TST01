bfba79c801809b6ada83f82224220d8d41309eda
Biel <biel@Biels-MacBook-Air.local>
2024-09-29 20:07:06 +0200

    Fixing connection problem

--- odooconection.py
+++ odooconection.py
@@ -1,10 +1,25 @@
 from flask import Flask, jsonify, request
+import os
 from flask_cors import CORS
 
 import xmlrpc.client
 
 app = Flask(__name__)
 CORS(app)
+
+
+@app.route('/')
+def home():
+    return "Hello, the server is live!"
+
+@app.route('/order-status')
+def order_status():
+    # Your logic here
+    return "Order status route"
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))  # Render provides the port as an environment variable
+    app.run(host='0.0.0.0', port=port)
 
 url = 'https://test-company47.odoo.com'
 db = 'test-company47'
