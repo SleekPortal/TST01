c281ba536b2e4a7edb226adfc5e0829bec6430a0
Biel <biel@Biels-MacBook-Air.local>
2024-10-04 19:43:59 +0200

    Think I found the problem

--- odooconection.py
+++ odooconection.py
@@ -1,78 +1,74 @@
 from flask import Flask, jsonify, request
 import os
 from flask_cors import CORS
-
 import xmlrpc.client
 
+# Initialize Flask app and enable CORS
 app = Flask(__name__)
 CORS(app)
 
-
-@app.route('/')
-def home():
-    return "Hello, the server is live!"
-
-@app.route('/order-status')
-def order_status():
-    # Your logic here
-    return "Order status route"
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))  # Render provides the port as an environment variable
-    app.run(host='0.0.0.0', port=port)
-
+# Odoo Connection Setup
 url = 'https://test-company47.odoo.com'
 db = 'test-company47'
 username = 'lazypau@protonmail.com'
 password = 'mainadmin'
 
+# Connect to Odoo via XML-RPC
 common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
 common.version()
 uid = common.authenticate(db, username, password, {})
 models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
-models.execute_kw(db, uid, password, 'res.partner', 'check_access_rights', ['read'], {'raise_exception': False})
-uid = common.authenticate(db, username, password, {})
 
+# Check read access on res.partner
+models.execute_kw(db, uid, password, 'res.partner', 'check_access_rights', ['read'], {'raise_exception': False})
+
+@app.route('/')
+def home():
+    return "Hello, the server is live!"
 
 @app.route('/order-status', methods=['GET'])
 def get_order_status():
+    # Get the order name from the request
     order_name = request.args.get('order_name')
 
+    # Fetch the status of the order from Odoo
     RawStatus = models.execute_kw(db, uid, password, 'stock.picking', 'search_read',
-                                [[['origin', '=', order_name]]],
-                                {'fields': ['state']})
+                                  [[['origin', '=', order_name]]],
+                                  {'fields': ['state']})
 
-    Product_list = models.execute_kw(db, uid, password, 'sale.order.line', 'search_read', 
-                                            [[['order_id', '=', order_name]]], 
-                                            {'fields': ['product_id','name', 'product_uom_qty']})
+    # Fetch the products of the order from Odoo
+    Product_list = models.execute_kw(db, uid, password, 'sale.order.line', 'search_read',
+                                     [[['order_id', '=', order_name]]],
+                                     {'fields': ['product_id', 'name', 'product_uom_qty']})
 
+    # Sort products by name
     Sorted_list = sorted(Product_list, key=lambda d: d['name'])
 
+    # Determine the delivery status and prepare the response
     if RawStatus:
         DeliveryStatus = RawStatus[0]['state']
-        print(DeliveryStatus)
         if DeliveryStatus == 'assigned':
             estado = 'preparacion'
             informacion = {}
-        
         elif DeliveryStatus == 'done':
             estado = 'enviado'
-            print("Enviado")
             ShippingInfo = models.execute_kw(db, uid, password, 'stock.picking', 'search_read',
-                                    [[['origin', '=', order_name]]],
-                                    {'fields': ['carrier_id','carrier_tracking_ref']})
-            if ShippingInfo: informacion ={'carrier': ShippingInfo[0]['carrier_id'][1],'tracking': ShippingInfo[0]['carrier_tracking_ref']}
+                                             [[['origin', '=', order_name]]],
+                                             {'fields': ['carrier_id', 'carrier_tracking_ref']})
+            if ShippingInfo:
+                informacion = {'carrier': ShippingInfo[0]['carrier_id'][1], 'tracking': ShippingInfo[0]['carrier_tracking_ref']}
+            else:
+                informacion = {}
         else:
             estado = 'esperando'
             informacion = {}
     else:
         estado = 'not_found'
         informacion = {}
-    
+
     return jsonify({'estado': estado, 'informacion': informacion})
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
+# Run the Flask server
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))  # Render provides the port as an environment variable
+    app.run(host='0.0.0.0', port=port)
