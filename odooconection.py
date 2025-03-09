c2869a5a486f3150fe4d08c873b6ef505d353ae3
Lazypau <99010107+Lazypau@users.noreply.github.com>
2025-03-09 12:22:46 +0100

    CORS bug fix

--- odooconection.py
+++ odooconection.py
@@ -7,7 +7,7 @@ import re
 
 # Initialize Flask app and enable CORS
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://lazypau.github.io"}})
+CORS(app, resources={r"/*": {"origins": "https://sleekportal.github.io"}})
 
 # Fetch Odoo credentials from environment variables
 url = os.environ.get('ODOO_URL')
